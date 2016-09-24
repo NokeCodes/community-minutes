a9e04280ee8deecb7f9413ffc402a96e45896c9c
Matthew Dunbar <matthewdunbar11@gmail.com>
2016-09-24 14:37:46 -0400

    Begins a basic C# program

---
+++ solr/DataImport/MeetingNote.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace DataImport
+{
+    class MeetingNote {
+        public string FullText { get; set; }
+        public DateTime MeetingDate { get; set; }
+    }    
+}
--- solr/DataImport/Program.cs
+++ solr/DataImport/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace DataImport
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var node = new Uri("http://127.0.0.1:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            MeetingNote meeting = new MeetingNote()
+            {
+                FullText = "This is a sample meeting note",
+                MeetingDate = DateTime.Now
+
+            };
+
+            var response = client.Index(meeting, idx => idx.Index("meetingNotes"));
+            Console.WriteLine(response);
         }
     }
 }
