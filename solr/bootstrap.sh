557e5acace1bc7882d36fa7324fb1942f5a521ac
Matthew Dunbar <matthewdunbar11@gmail.com>
2016-09-24 12:52:58 -0400

    Fixes bootstrap.sh

--- solr/bootstrap.sh
+++ solr/bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get purge openjdk*
 sudo apt-get -y install openjdk-7-jdk
 # install ES
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list"
 sudo apt-get update && sudo apt-get install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
 sudo /etc/init.d/elasticsearch start
